c001cf4b3e3bbb736e8fccd8e215f6bf2f8b5eb0
Scott Baker <sbaker@trucktrax.com>
2024-09-27 09:14:17 -0700

    Add project files.

---
+++ CodeTimer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UtiliDude
+{
+    public class CodeTimer : IDisposable
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly string _name;
+        private static readonly object _lock = new object();
+
+        private CodeTimer(string name)
+        {
+            _name = string.IsNullOrWhiteSpace(name)
+                ? throw new ArgumentException($"'{nameof(name)}' cannot be null or whitespace.", nameof(name))
+                : name;
+
+            _stopwatch = Stopwatch.StartNew();
+
+            Debug.WriteLine($" →→ Starting {_name} timer.");
+            Lock(() => Debug.Indent());
+        }
+
+        public static IDisposable Start(string name) => new CodeTimer(name);
+
+        public void Stop()
+        {
+            _stopwatch.Stop();
+            Lock(() => Debug.Unindent());
+            Debug.WriteLine($" ←← {_name} took {_stopwatch.ElapsedMilliseconds}ms.");
+        }
+
+        public void Dispose() => Stop();
+
+        private void Lock(Action action)
+        {
+            lock (_lock) { action(); }
+        }
+    }
+}
